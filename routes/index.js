0b664836638244b8ec987283bc82c3ede50cf1a9
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-12 16:06:32 -0600

    add routes to login

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+//@description  Login/Landing Page
+//@route        GET /
+router.get('/', (req, res)=>{
+    res.render('login')
+})
+
+//@description      Dashboard
+//@route            GET /dashboard
+router.get('/dashboard', (req,res)=>{
+    res.render('dashboard')
+})
+
+
+//export this router
+module.exports = router
