5e02c73e5192c1826529694dc5390f3f17943497
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-14 19:09:44 -0600

    add secure routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 //@description  Login/Landing Page
 //@route        GET /
-router.get('/', (req, res)=>{
-    res.render('login')
+//add middleware as a second argument
+router.get('/', ensureGuest, (req, res)=>{
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
 //@description      Dashboard
 //@route            GET /dashboard
-router.get('/dashboard', (req,res)=>{
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, (req,res)=>{
+    res.render('dashboard', {
+        name: req.user.firstName,
+    })
 })
 
 
