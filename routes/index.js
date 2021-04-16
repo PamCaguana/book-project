cd6426e525e7f6c61b1b724261bd27a3dd8d83f4
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-15 21:27:14 -0600

    create route for stories in dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //@description  Login/Landing Page
 //@route        GET /
 //add middleware as a second argument
@@ -13,10 +15,19 @@ router.get('/', ensureGuest, (req, res)=>{
 
 //@description      Dashboard
 //@route            GET /dashboard
-router.get('/dashboard', ensureAuth, (req,res)=>{
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req,res)=>{
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        //documents returned from queries with lean option enabled are plain JS objects, not MongooseDocuments
+        //they have no save method, getters/setters or other Mongoose magil applied
+        res.render('dashboard', {
+            name: req.user.firstName,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+    
 })
 
 
